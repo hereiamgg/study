c55a521322f351ec8a749673c2539337688d452d
hereiamgg <22579376@qq.com>
2022-03-06 20:25:53 +0800

    v0.1

---
+++ homework2/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	// NewServerMux
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/healthz", healthz)
+
+	// start
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("start http server failed,error:%s\n", err.Error())
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	//接收客户端 request，并将 request 中带的 header 写入 response header
+	for k, v := range r.Header {
+		for _, vv := range v {
+			fmt.Printf("Header key: %s, value: %s \n", k, v)
+			w.Header().Set(k, vv)
+		}
+	}
+
+	//读取当前系统的环境变量中的 VERSION 配置，并写入 response header
+	os.Setenv("VERSION", "v1.0")
+	version := os.Getenv("VERSION")
+	w.Header().Set("VERSION", version)
+	fmt.Printf("os version: %s \n", version)
+
+	//Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
+	clientip := ClientIP(r)
+	log.Printf("client ip : %s", clientip)
+	log.Printf("response:%d", 200)
+}
+
+func ClientIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok!")
+}
